42316143f9d263f99e12f6d38787e4bb7103e85a
M-HAmo <mohamed.kandail@yahoo.com>
2020-06-07 21:44:54 +0200

    add audio in the background for fun

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ $(window).scroll(function () {
         $('.navbar').addClass('fixed');
     }
 });
+window.onload =  () =>{
+    var myAudio = document.getElementById('my-audio');
+    var play = document.getElementById('play');
+    var pause = document.getElementById('pause');
+    
+    {
+        myAudio.play();
+
+    }    
+    play.onclick = () => {
+        myAudio.play();
+        return false;
+        
+    }
+    pause.onclick = () => {
+        myAudio.pause();
+        return false;
+    }
+}
